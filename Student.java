c2837c7c4e15e2b5002dd736e74af9c9231130c3
onur_vrs <varisonur0@gmail.com>
2025-12-15 16:49:29 +0300

    Student class added

---
+++ Student.java
@@ -0,0 +1,21 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Student implements Registrable {
+    private String name;
+    private String surname;
+    private String id;
+    private boolean registered = false;
+
+    private Map<String,Course>dailyCourses =new LinkedHashMap<>();
+
+    private Map<String, String> selectedInstructors = new LinkedHashMap<>();
+
+    private Map<Course, Double> grades = new LinkedHashMap<>();
+
+    public Student(String name,String surname,String id){
+        this.name=name;
+        this.surname=surname;
+        this.id=id;
+    }
+}
