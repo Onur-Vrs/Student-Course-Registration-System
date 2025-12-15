b2a049551e9ac41110550f7ef595407ebb7173a6
onur_vrs <varisonur0@gmail.com>
2025-12-15 16:27:19 +0300

    Course Class Upgrade and fix

--- Course.java
+++ Course.java
@@ -1,20 +1,30 @@
+import java.util.List;
+
 public class Course {
     private String courseName;
     private String courseNumber;
     private int credit;
+    private List<String> instructors;
+    private String timeSlot; 
 
-    public Course(String courseName, String courseNumber, int credit) {
+    public Course(String courseName, String courseNumber, int credit,
+                  List<String> instructors, String timeSlot) {
         this.courseName = courseName;
         this.courseNumber = courseNumber;
         this.credit = credit;
+        this.instructors = instructors;
+        this.timeSlot = timeSlot;
     }
 
     public String getCourseName() { return courseName; }
     public String getCourseNumber() { return courseNumber; }
     public int getCredit() { return credit; }
+    public List<String> getInstructors() { return instructors; }
+    public String getTimeSlot() { return timeSlot; }
 
     @Override
     public String toString() {
-        return courseName + " (" + courseNumber + ") - Credit: " + credit;
+        return courseName + " (" + courseNumber + ") - Credit: " + credit
+                + " | Time: " + timeSlot + " | Instructors: " + instructors;
     }
 }
